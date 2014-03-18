cc94c81c39675a8a947572ab56c8e1a0dc792598
Nick Semenkovich <nick@semenkovich.com>
2014-03-18 16:20:53 -0500

    Bump to 21 days, for convenience as default (if deployed as enterprise policy).

--- background.js
+++ background.js
@@ -10,8 +10,8 @@
 "use strict";
 
 function scanCookies() {
-    // Grab the cookie preference, or default to 10 days.
-    var maxCookieLifeInDays = JSON.parse(localStorage.cookieLimit || 10);
+    // Grab the cookie preference, or default to 21 days.
+    var maxCookieLifeInDays = JSON.parse(localStorage.cookieLimit || 21);
 
     var maxCookieLifeInSeconds = maxCookieLifeInDays * 24 * 60 * 60;
 
--- options.js
+++ options.js
@@ -13,8 +13,8 @@ for (var i=1; i<=60; i++){
     select.options[select.options.length] = new Option(i);
 }
 
-// Default to 10 if LSO isn't set
-var cookieLimit = JSON.parse(localStorage.cookieLimit || 10);
+// Default to 21 if LSO isn't set
+var cookieLimit = JSON.parse(localStorage.cookieLimit || 21);
 select.selectedIndex = cookieLimit - 1;  // Zero indexed.
 
 
